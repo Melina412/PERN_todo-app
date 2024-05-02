b6b5c32273422fbca692f66ab944bf016be18dba
Melina412 <melina.schild@icloud.com>
2024-05-02 16:01:21 +0200

    psql connection string & env

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,9 +3,5 @@ const { Pool } = pkg;
 import 'dotenv/config';
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DB_URL,
 });

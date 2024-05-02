7b46b03ea9b2278678301626660f4a121d55777c
Melina412 <melina.schild@icloud.com>
2024-05-02 15:15:36 +0200

    static index file setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import { app } from './src/app.js';
 
 const PORT = process.env.PORT;
 
+const FRONTEND_INDEX = new URL('./frontend/dist/index.html', import.meta.url)
+  .pathname;
+
+app.get('*', (_, res) => {
+  res.sendFile(FRONTEND_INDEX);
+});
+
 app.get('/', (_, res) => {
   res.json({ message: `express server on port ${PORT} ✅` });
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import morgan from 'morgan';
-// import { pool } from '../db.js';
 
 import { router as todoRouter } from './todos/todos.router.js';
 
@@ -11,5 +10,8 @@ export const app = express();
 app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
 app.use(express.json());
 app.use(morgan('dev'));
+
+const FRONTEND_DIR = new URL('../frontend/dist', import.meta.url).pathname;
+app.use(express.static(FRONTEND_DIR));
 
 app.use('/api/todos', todoRouter);
